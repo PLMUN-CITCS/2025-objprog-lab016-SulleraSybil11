3b0ae1ebe74a0d58d241ce67852ce608ed3387c1
SulleraSybil11 <sullerasybil_bscs@plmun.edu.ph>
2025-02-20 18:38:21 +0800

    Submitting OBJPROG Week 05 - Laboratory # 16

---
+++ MethodOverloading.java
@@ -0,0 +1,15 @@
+public class MethodOverloading {
+    public static void main(String[] args) {  //Defining the main method
+
+        int intValue = 10;  //Declaring int variable
+        double doubleValue = 3.14;  //Declaring double variable
+        String stringValue = "Hello!";  //Declaring string variable
+
+//Printing all the declared variables
+System.out.println("Integer value: " + intValue);
+System.out.println("Double value: " + doubleValue);
+System.out.println("String value: " + stringValue);
+
+
+    }
+}
